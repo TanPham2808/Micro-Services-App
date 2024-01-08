ed40a8f2d51751355ed2f811aae8ab5faf402156
Tan Pham <tan.phamx@dmchosting.ca>
2024-01-08 16:14:45 +0700

    Create Mango.Web

---
+++ Mango.Web/Models/ReqestDTO.cs
@@ -0,0 +1,12 @@
+﻿using static Mango.Web.Utility.SD;
+
+namespace Mango.Web.Models
+{
+    public class ReqestDTO
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public string Data { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
