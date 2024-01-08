e35fe2f80fd8c499653bd0b2a87cba627a06122f
Tan Pham <tan.phamx@dmchosting.ca>
2024-01-08 13:50:13 +0700

    Setup Nuget cho CouponAPI

---
+++ Mango.Services.CouponAPI/Models/Coupon.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.Services.CouponAPI.Models
+{
+    public class Coupon
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public int MinAmout { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
---
+++ Mango.Services.CouponAPI/Models/DTO/CouponDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Services.CouponAPI.Models.DTO
+{
+    public class CouponDTO
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public int MinAmout { get; set; }
+    }
+}
