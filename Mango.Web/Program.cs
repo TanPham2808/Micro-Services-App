c9bdfc63d9a13d8b9628acbc02af4284112e7359
Tan Pham <tan.phamx@dmchosting.ca>
2024-01-09 10:08:30 +0700

    Đăng ký services trong program file

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -1,7 +1,21 @@
+﻿using Mango.Web.Service;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpContextAccessor(); // Dịch vụ này cung cấp quyền truy cập vào ngữ cảnh HTTP hiện tại, bao gồm thông tin về yêu cầu và phản hồi.
+builder.Services.AddHttpClient(); // Cho phép bạn cấu hình và sử dụng các phiên bản HttpClient trong ứng dụng của bạn để thực hiện các yêu cầu HTTP đến các dịch vụ hoặc API khác.
+builder.Services.AddHttpClient();
+builder.Services.AddHttpClient<ICouponService, CouponService>();
+
+SD.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"]; // Gán giá trị từ config vào biến
+
+builder.Services.AddScoped<IBaseService, BaseService>();
+builder.Services.AddScoped<ICouponService, CouponService>();
 
 var app = builder.Build();
 
