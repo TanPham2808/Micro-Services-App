a15effb6be5c26e2296bf01f5f57260cbf9c2503
Tan Pham <tan.phamx@dmchosting.ca>
2024-01-09 17:15:53 +0700

    Khởi tạo các DTO cần thiết cho AuthenAPI

---
+++ Mango.Services.CouponAPI/Models/DTO/LoginRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Mango.Services.CouponAPI.Models.DTO
+{
+    public class LoginRequestDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
