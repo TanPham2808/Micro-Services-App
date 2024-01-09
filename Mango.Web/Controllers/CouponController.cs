f7cdbd9765044ec48ce6b28242f4bd14598c4447
Tan Pham <tan.phamx@dmchosting.ca>
2024-01-09 11:18:07 +0700

    Cotroller call qua API

---
+++ Mango.Web/Controllers/CouponController.cs
@@ -0,0 +1,27 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+        public async Task<IActionResult> CouponIndex()
+        {
+            ResponseDTO respone = await _couponService.GetAllCouponAsync();
+            List<CouponDTO> lstCoupon = null;
+            if (respone.IsSuccess)
+            {
+                lstCoupon = JsonConvert.DeserializeObject<List<CouponDTO>>(Convert.ToString(respone.Result));
+            }
+
+            return View(lstCoupon);
+        }
+    }
+}
