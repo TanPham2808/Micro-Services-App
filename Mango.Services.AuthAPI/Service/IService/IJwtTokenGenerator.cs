5b0668f1974cff4b9762cea173963957c216cc73
Tan Pham <tan.phamx@dmchosting.ca>
2024-01-10 16:37:43 +0700

    Generate Token

---
+++ Mango.Services.AuthAPI/Service/IService/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using Mango.Services.AuthAPI.Model;
+
+namespace Mango.Services.AuthAPI.Service.IService
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(ApplicationUser applicationUser);
+    }
+}
---
+++ Mango.Services.AuthAPI/Service/JwtTokenGenerator.cs
@@ -0,0 +1,45 @@
+﻿using Mango.Services.AuthAPI.Model;
+using Mango.Services.AuthAPI.Service.IService;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Mango.Services.AuthAPI.Service
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly JWTOption _jWTOption;
+        public JwtTokenGenerator(JWTOption jWTOption)
+        {
+            _jWTOption = jWTOption;
+        }
+        public string GenerateToken(ApplicationUser applicationUser)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            
+            var keySecrect= Encoding.ASCII.GetBytes(_jWTOption.Secret);
+
+            var claimList = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Email,applicationUser.Name),
+                new Claim(JwtRegisteredClaimNames.Sub,applicationUser.Id),
+                new Claim(JwtRegisteredClaimNames.Name,applicationUser.UserName)
+            };
+
+            var tokenDescription = new SecurityTokenDescriptor
+            {
+                Audience = _jWTOption.Audience,
+                Issuer = _jWTOption.Issuer,
+                Subject = new ClaimsIdentity(claimList),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(keySecrect), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescription);
+
+            return tokenHandler.WriteToken(token);
+
+        }
+    }
+}
