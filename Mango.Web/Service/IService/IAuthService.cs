9bb034f4b23feb67465d0dd9237fbbee73c742d4
Tan Pham <tan.phamx@dmchosting.ca>
2024-01-11 10:37:16 +0700

    Khởi tạo AuthService cho WebProject

---
+++ Mango.Web/Service/IService/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<ResponseDTO> LoginAsync(LoginRequestDTO loginRequestDTO);
+        Task<ResponseDTO> RegisterAsync(RegisterationRequestDTO registerationRequestDTO);
+        Task<ResponseDTO> AssignAsync(RegisterationRequestDTO registerationRequestDTO);
+    }
+}
