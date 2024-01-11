b2ff09d43ffcb40da0734265e7ba1eaace291183
Tan Pham <tan.phamx@dmchosting.ca>
2024-01-11 15:46:45 +0700

    - Khởi tạo IHttpContextAccessor - Khởi tạo Service TokenProvider

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -9,7 +9,10 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddHttpContextAccessor(); // Dịch vụ này cung cấp quyền truy cập vào ngữ cảnh HTTP hiện tại, bao gồm thông tin về yêu cầu và phản hồi.
 builder.Services.AddHttpClient(); // Cho phép bạn cấu hình và sử dụng các phiên bản HttpClient trong ứng dụng của bạn để thực hiện các yêu cầu HTTP đến các dịch vụ hoặc API khác.
+
+// AddHttpClient được sử dụng để đăng ký một dịch vụ HTTP client trong container dịch vụ của ứng dụng
 builder.Services.AddHttpClient<ICouponService, CouponService>();
+builder.Services.AddHttpClient<IAuthService, AuthService>();
 
 // Gán string localhost:700x từ config vào string để bên trong lấy ra sử dụng 
 SD.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"]; 
@@ -18,6 +21,7 @@ SD.AuthAPIBase = builder.Configuration["ServiceUrls:AuthAPI"];
 builder.Services.AddScoped<IBaseService, BaseService>();
 builder.Services.AddScoped<ICouponService, CouponService>();
 builder.Services.AddScoped<IAuthService,AuthService>();
+builder.Services.AddScoped<ITokenProvider, TokenProvider>();
 
 var app = builder.Build();
 
