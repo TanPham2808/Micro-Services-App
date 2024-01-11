623f4eb24c15a34dda612b46b2c6245bffbb0f73
Tan Pham <tan.phamx@dmchosting.ca>
2024-01-11 13:52:00 +0700

    Dựng UI Login và Register page

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -1,6 +1,8 @@
 ﻿using Mango.Web.Models;
 using Mango.Web.Service.IService;
+using Mango.Web.Utility;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Mango.Web.Controllers
 {
@@ -24,8 +26,14 @@ namespace Mango.Web.Controllers
         [HttpGet]
         public IActionResult Register()
         {
-            RegisterationRequestDTO registerationRequestDTO = new();
-            return View(registerationRequestDTO);
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem{Text=SD.RoleAdmin, Value=SD.RoleAdmin},
+                new SelectListItem{Text=SD.RoleCustomer, Value=SD.RoleCustomer},
+            };
+            ViewBag.RoleList = roleList;
+
+            return View();
         }
 
         [HttpGet]
