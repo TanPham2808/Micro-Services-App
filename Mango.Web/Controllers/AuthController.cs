7d9cda79a9b687a4f21f8a04802f7e6a3a4000fe
Tan Pham <tan.phamx@dmchosting.ca>
2024-01-11 10:58:59 +0700

    Dựng Auth Controller

---
+++ Mango.Web/Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDTO loginRequestDTO = new();
+            
+            return View(loginRequestDTO);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            RegisterationRequestDTO registerationRequestDTO = new();
+            return View(registerationRequestDTO);
+        }
+
+        [HttpGet]
+        public IActionResult Assign()
+        {
+            RegisterationRequestDTO registerationRequestDTO = new();
+            return View(registerationRequestDTO);
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
