707e94eb284d7823700aa03c1afc6b99a49e43df
Tan Pham <tan.phamx@dmchosting.ca>
2024-01-12 11:16:33 +0700

    Tạo dự án Services Product API

---
+++ Mango.Services.ProductAPI/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Mango.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
