3c7ff9f4b82517969896d899a9913d15bedb3d55
Tan Pham <tan.phamx@dmchosting.ca>
2024-01-12 14:25:30 +0700

    Xây dựng mapper và CRUD cho Product API

--- Mango.Services.ProductAPI/Program.cs
+++ Mango.Services.ProductAPI/Program.cs
@@ -1,6 +1,7 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Mango.Services.ProductAPI.Data;
-
+using Mango.Services.ProductAPI;
+using AutoMapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,14 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+#region Setup Auto Mapper
+// Tạo 1 class MappingConfig
+IMapper mapper = MappingConfig.RegisterMap().CreateMapper();
+
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+#endregion
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
