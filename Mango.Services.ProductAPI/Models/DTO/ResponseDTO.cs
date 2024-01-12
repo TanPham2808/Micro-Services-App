55ff8a17dfe39ac005414b14410523f0b0351b9d
TANPHAM\xuant <xuantan.pham2808@gmail.com>
2024-01-13 00:48:41 +0700

    Xóa folder ko dùng cho dự án

--- Mango.Services.ProductAPI/Models/DTO/ResponseDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Mango.Services.ProductAPI.Models.DTO
-{
-    public class ResponseDTO
-    {
-        public object? Result { get; set; }
-        public bool IsSuccess { get; set; } = true;
-        public string Message { get; set; } = "";
-    }
-}
