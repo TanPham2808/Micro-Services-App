e9c7a1e0c2534087c11b6c19d64f67457dcffe78
TANPHAM\xuant <xuantan.pham2808@gmail.com>
2024-01-12 22:23:54 +0700

    Dựng lại source ProductsAPI do thiếu file ko load được project

---
+++ Mango.Services.ProductsAPI/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Mango.Services.ProductsAPI.Models;
+using Mango.Services.ProductsAPI.Models.DTO;
+
+namespace Mango.Services.ProductsAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMap()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDTO, Product>();
+                config.CreateMap<Product, ProductDTO>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
