b5c0bec142062c8ac425c77517680c6a6d23c4b8
Tan Pham <tan.phamx@dmchosting.ca>
2024-01-15 15:08:50 +0700

    Thêm Model và tạo table Email

---
+++ Mango.Services.EmailAPI/Models/DTO/CartDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Mango.Services.EmailAPI.Models
+{
+    public class CartDTO
+    {
+        public CartHeaderDTO CartHeader { get; set; }
+        public IEnumerable<CartDetailDTO> CartDetails { get; set; }
+    }
+}
---
+++ Mango.Services.EmailAPI/Models/DTO/CartHeaderDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.EmailAPI.Models
+{
+    public class CartHeaderDTO
+    {
+        public int CartHeaderId { get; set; }
+        public string? UserId { get; set; }
+        public string? CouponCode { get; set; }
+        public double Discount { get; set; }
+        public double CartTotal { get; set; }
+
+        // Thông tin lưu trữ thêm
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+    }
+}
