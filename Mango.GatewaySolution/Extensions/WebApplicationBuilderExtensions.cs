f5012e5d7a1eb14cccbc7770e33dcb57dce784be
Tan Pham <tan.phamx@dmchosting.ca>
2024-01-16 13:45:58 +0700

    Khởi tạo gateway project

---
+++ Mango.GatewaySolution/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Mango.GatewaySolution
+{
+    public static class WebApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// Authentication & Authorization call API
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static WebApplicationBuilder AddAppAuthentication(this WebApplicationBuilder builder)
+        {
+            // Xác thực API
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidAudience = builder.Configuration["ApiSettings:Audience"],
+                        ValidIssuer = builder.Configuration["ApiSettings:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["ApiSettings:Secret"]))
+                    };
+                });
+            
+            return builder;
+        }
+    }
+}
---
+++ Mango.GatewaySolution/Program.cs
@@ -0,0 +1,14 @@
+using Mango.GatewaySolution;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+// Nhúng extentions
+builder.AddAppAuthentication();
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.UseOcelot();
+app.Run();
