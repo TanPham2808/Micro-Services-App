eecbe82de11041b7396f7cfc3468b4b2d7c43788
TANPHAM\xuant <xuantan.pham2808@gmail.com>
2024-01-16 22:03:23 +0700

    Clean source ocelot

--- Mango.GatewaySolution/Program.cs
+++ Mango.GatewaySolution/Program.cs
@@ -5,10 +5,12 @@ using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 // Nhúng extentions
 builder.AddAppAuthentication();
-builder.Services.AddOcelot();
+// Nhung file ocelot json bên ngoài vào
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.UseOcelot();
+app.UseOcelot().GetAwaiter().GetResult();
 app.Run();
