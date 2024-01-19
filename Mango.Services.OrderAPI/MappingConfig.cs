2ef20e4887d7ee4f1bb1b66361a5f903223a22cc
TANPHAM\xuant <xuantan.pham2808@gmail.com>
2024-01-19 20:11:29 +0700

    Config mapping

--- Mango.Services.OrderAPI/MappingConfig.cs
+++ Mango.Services.OrderAPI/MappingConfig.cs
@@ -10,6 +10,15 @@ namespace Mango.Services.OrderAPI
         {
             var mappingConfig = new MapperConfiguration(config =>
             {
+                config.CreateMap<OrderHeaderDTO, CartHeaderDTO>()
+                .ForMember(dest => dest.CartTotal, u => u.MapFrom(src => src.OrderTotal)).ReverseMap();
+
+                config.CreateMap<CartDetailDTO, OrderDetailDTO>()
+                .ForMember(dest => dest.ProductName, u => u.MapFrom(src => src.Product.Name))
+                .ForMember(dest => dest.Price, u => u.MapFrom(src => src.Product.Price));
+
+                config.CreateMap<OrderDetailDTO, CartDetailDTO>();
+
                 config.CreateMap<OrderHeader, OrderHeaderDTO>().ReverseMap();
                 config.CreateMap<OrderDetail, OrderDetailDTO>().ReverseMap();
             });
